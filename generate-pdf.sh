c9cbc44f8c591d25dd44255aea192d63e259b52d
antonio <antonio.medeiros@estudante.ufjf.br>
2022-07-06 17:41:40 -0300

    script para gerar pdf do relatorio com pandoc

---
+++ generate-pdf.sh
@@ -0,0 +1,2 @@
+# sudo apt install pandoc texlive-latex-base texlive-fonts-recommended texlive-extra-utils   texlive-latex-extra   texlive-xetex
+pandoc relatorio.md -o Relatorio-Lista-1.pdf --pdf-engine=xelatex
